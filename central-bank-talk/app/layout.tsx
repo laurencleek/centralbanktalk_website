405992cc560e1de8850e9024a399ea9c0026f06a
Simeon Bischl <hallo@simeonbischl.de>
2024-11-14 16:00:01 +0100

    Add link back to homepage

--- central-bank-talk/app/layout.tsx
+++ central-bank-talk/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
             <header className="sticky top-0 z-50 w-full border-b border-slate-200 bg-white shadow-sm">
               <div className="container mx-auto px-4">
                 <div className="flex h-16 items-center justify-between">
-                  <div className="flex items-center gap-4">
+                  <Link href="/" className="flex items-center gap-4">
                     <div className="relative w-10 h-10 overflow-hidden rounded-full">
                       <Image
                         src="logo.webp"
@@ -45,7 +45,7 @@ export default function RootLayout({
                       <span className="text-black">CB</span>
                       <span className="text-blue-950"> Talk</span>
                     </span>
-                  </div>
+                  </Link>
                   <nav className="flex items-center">
                     {[
                       { name: "Data", href: "/data-page" },
